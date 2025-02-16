258f740b22df216432a51531578af8912df2a9f5
Chaim-Pollak <ch324id@gmail.com>
2025-02-16 16:23:34 +0200

    Refactor IssueModal and EmployeeModal components for improved clarity and consistency

--- src/components/modal/IssueModal.jsx
+++ src/components/modal/IssueModal.jsx
@@ -4,27 +4,15 @@ import { useContext } from "react";
 import { ActionContext } from "../contexts/ActionContext";
 
 function IssueModal() {
-  const { setIss } = useContext(ActionContext);
+  const { setActiveIssue } = useContext(ActionContext);
 
   function handleCancel() {
     document.getElementById("issue_modal").close();
-    setIss(null);
+    setActiveIssue(null);
   }
   return (
     <dialog id="issue_modal" className="modal">
-      <div className="modal-box w-[60%] h-[80%] max-w-none max-h-none p-0">
-        {/* Exit Btn */}
-        <div>
-          <button
-            onClick={handleCancel}
-            className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
-          >
-            ✕
-          </button>
-        </div>
-
-        <IssueForm />
-      </div>
+      <IssueForm />
     </dialog>
   );
 }
--- src/components/modal/employeeModal.jsx
+++ src/components/modal/employeeModal.jsx
@@ -1,23 +1,9 @@
-import React from "react";
-
 import EditEmployeeForm from "../pages/forms/EditEmployeeForm";
 
 function EmployeeModal() {
   return (
     <dialog id="employee_modal" className="modal">
-      <div className="modal-box p-0">
-        {/* Exit Btn */}
-        {/* <div>
-          <button
-            onClick={() => document.getElementById("employee_modal").close()}
-            className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
-          >
-            ✕
-          </button>
-        </div> */}
-
-        <EditEmployeeForm />
-      </div>
+      <EditEmployeeForm />
     </dialog>
   );
 }
